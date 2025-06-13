5eb6cc270e9e506a5349baff1048e4c2bec31f39
Eoloth <ruben.msalles@gmail.com>
2025-06-13 15:34:15 -0400

    Añadido saludo

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"mensaje": "!Bienvenido a tu priemra API con FastAPI"}
+    return {"mensaje": "!Bienvenido a tu primera API con FastAPI!"}
+
+@app.get("/saludo/{nombre}")
+def saludar(nombre: str):
+    return {"mensaje:" f"Hola, {nombre}. Bienvenido a FastApi 👋"}
