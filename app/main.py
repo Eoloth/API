943fdb0a4332b6eb5fb559ed9594b047430985b3
Eoloth <ruben.msalles@gmail.com>
2025-06-14 15:32:36 -0400

    Actualizado routes.py con descripcion

--- app/main.py
+++ app/main.py
@@ -3,6 +3,15 @@
 from fastapi import FastAPI
 from app.routes.routes import router
 
-app = FastAPI(title="Pokémon API + Registro de Usuarios")
+app = FastAPI(
+    title="API de Rubén Manríquez",
+    description="Proyecto de aprendizaje con FastAPI, integrando validaciones, rutas y APIs externas.",
+    version="1.0.0",
+    contact={
+        "name": "Rubén Manríquez",
+        "url": "https://rubenmanriquez.github.io",
+        "email": "ruben.msalles@gmail.com"
+    },
+    )
 
 app.include_router(router)
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -19,7 +19,7 @@ usuarios = {
     "admin": pwd_context.hash("admin123")
 }
 
-@router.post("/login")
+@router.post("/login", summary="Autenticación con token JWT", description="Valida usuario y contraseña. Si son correctos, genera y devuelve un token JWT.")
 def login(credenciales: Credenciales):
     user = credenciales.username
     password = credenciales.password
@@ -30,25 +30,25 @@ def login(credenciales: Credenciales):
     token = crear_token({"sub": user})
     return {"access_token": token, "token_type": "bearer"}
 
-@router.get("/usuario/protegido")
+@router.get("/usuario/protegido", summary="Ruta protegida con JWT", description="Devuelve un mensaje de acceso permitido solo si se presenta un token JWT válido.")
 def ruta_protegida(token: str = Depends(oauth2_scheme)):
     payload = verificar_token(token)
     return {"usuario": payload["sub"], "mensaje": "Acceso permitido"}
 
-@router.get("/")
+@router.get("/", summary="Inicio de la API", description="Retorna un mensaje de bienvenida a la API.")
 def root():
     return {"mensaje": "¡Bienvenido a tu primera API con FastAPI!"}
 
-@router.get("/saludo/{nombre}")
+@router.get("/saludo/{nombre}", summary="Saludo personalizado", description="Devuelve un saludo para el nombre proporcionado en la URL.")
 def saludar(nombre: str):
     return {"mensaje": f"Hola, {nombre}. Bienvenido a FastAPI 👋"}
 
-@router.post("/registro")
+@router.post("/registro", summary="Registrar usuario", description="Registra un usuario validando que el nombre tenga solo letras y espacios.")
 def registrar_usuario(usuario: Usuario):
     if not re.fullmatch(r"^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$", usuario.nombre):
         raise HTTPException(status_code=400, detail="El nombre solo puede contener letras y espacios.")
     return JSONResponse(status_code=201, content={"mensaje": f"Usuario {usuario.nombre} registrado correctamente."})
 
-@router.get("/pokemon/{nombre}")
+@router.get("/pokemon/{nombre}", summary="Consultar Pokémon", description="Consulta datos de un Pokémon usando la PokeAPI externa. Devuelve nombre, altura, peso, tipo y habilidades.")
 def get_pokemon(nombre: str):
     return obtener_pokemon_info(nombre)
