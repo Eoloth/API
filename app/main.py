d3737e16ab65be4f11e5ab8d76fd37c4080122da
Eoloth <ruben.msalles@gmail.com>
2025-06-14 15:39:27 -0400

    Añadida descripcion a main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,13 @@ from app.routes.routes import router
 
 app = FastAPI(
     title="API de Rubén Manríquez",
-    description="Proyecto de aprendizaje con FastAPI, integrando validaciones, rutas y APIs externas.",
+    description="""
+    Proyecto de aprendizaje con FastAPI, integrando validaciones, rutas y APIs externas."
+
+    [Rubén Manríquez - Website](https://rubenmanriquez.github.io)
+    [Send email to Rubén Manríquez](mailto:ruben.msalles@gmail.com)
+    """,
+
     version="1.0.0",
     contact={
         "name": "Rubén Manríquez",
